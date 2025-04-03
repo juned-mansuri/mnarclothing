71294ea9b5f257977aab64eea397c288a66962c3
juned-mansuri <0studyforair@gmail.com>
2025-04-03 17:38:06 +0530

    address changes

--- frontend/src/pages/Terms.jsx
+++ frontend/src/pages/Terms.jsx
@@ -4,121 +4,199 @@ const Terms = () => {
   return (
     <div className="container mx-auto px-4 py-10 text-gray-800">
       <div className="max-w-4xl mx-auto">
-       <div>    
-       <h1 className="text-4xl font-bold text-center mb-6 text-gray-900">
-          Terms and Conditions
-        </h1>
-        <p className="mb-4 text-2xl text-gray-600   ">Last updated on Apr 1st 2025</p>
-        <p className="mb-4 text-lg">
-        For the purpose of these Terms and Conditions, The term "we", "us", "our" used anywhere on this page shall mean Mnar Clothing, whose registered/operational office is Kitiyani , Mandsaur Mandsaur MADHYA PRADESH 458001 . "you", “your”, "user", “visitor” shall mean any natural or legal person who is visiting our website and/or agreed to purchase from us.
-        </p>
+        <div>
+          <h1 className="text-4xl font-bold text-center mb-6 text-gray-900">
+            Terms and Conditions
+          </h1>
+          <p className="mb-4 text-2xl text-gray-600   ">
+            Last updated on Apr 1st 2025
+          </p>
+          <p className="mb-4 text-lg">
+            For the purpose of these Terms and Conditions, The term "we", "us",
+            "our" used anywhere on this page shall mean Mnar Clothing, whose
+            registered/operational office is 458001 , Building No - 47, Ward No
+            . 11 , Chakrawati Colony, Near Pamnani Hospital . "you", “your”,
+            "user", “visitor” shall mean any natural or legal person who is
+            visiting our website and/or agreed to purchase from us.
+          </p>
 
-        <div className="space-y-6">
-          <div>
-            <h2 className="text-2xl font-semibold text-gray-900">
-            Your use of the website and/or purchase from us are governed by following Terms and Conditions:
-            </h2>
-            <ul className="list-disc list-inside text-lg mt-2">
-              <li>
-              The content of the pages of this website is subject to change without notice.
-              </li>
-              <li>Neither we nor any third parties provide any warranty or guarantee as to the accuracy, timeliness, performance, completeness or suitability of the information and materials found or offered on this website for any particular purpose. You acknowledge that such information and materials may contain inaccuracies or errors and we expressly exclude liability for any such inaccuracies or errors to the fullest extent permitted by law.</li>
-              <li>
-              Your use of any information or materials on our website and/or product pages is entirely at your own risk, for which we shall not be liable. It shall be your own responsibility to ensure that any products, services or information available through our website and/or product pages meet your specific requirements.
-              </li>
-              <li>
-              Our website contains material which is owned by or licensed to us. This material includes, but are not limited to, the design, layout, look, appearance and graphics. Reproduction is prohibited other than in accordance with the copyright notice, which forms part of these terms and conditions.
-              </li>
-              <li>
-              All trademarks reproduced in our website which are not the property of, or licensed to, the operator are acknowledged on the website.
+          <div className="space-y-6">
+            <div>
+              <h2 className="text-2xl font-semibold text-gray-900">
+                Your use of the website and/or purchase from us are governed by
+                following Terms and Conditions:
+              </h2>
+              <ul className="list-disc list-inside text-lg mt-2">
+                <li>
+                  The content of the pages of this website is subject to change
+                  without notice.
+                </li>
+                <li>
+                  Neither we nor any third parties provide any warranty or
+                  guarantee as to the accuracy, timeliness, performance,
+                  completeness or suitability of the information and materials
+                  found or offered on this website for any particular purpose.
+                  You acknowledge that such information and materials may
+                  contain inaccuracies or errors and we expressly exclude
+                  liability for any such inaccuracies or errors to the fullest
+                  extent permitted by law.
+                </li>
+                <li>
+                  Your use of any information or materials on our website and/or
+                  product pages is entirely at your own risk, for which we shall
+                  not be liable. It shall be your own responsibility to ensure
+                  that any products, services or information available through
+                  our website and/or product pages meet your specific
+                  requirements.
+                </li>
+                <li>
+                  Our website contains material which is owned by or licensed to
+                  us. This material includes, but are not limited to, the
+                  design, layout, look, appearance and graphics. Reproduction is
+                  prohibited other than in accordance with the copyright notice,
+                  which forms part of these terms and conditions.
+                </li>
+                <li>
+                  All trademarks reproduced in our website which are not the
+                  property of, or licensed to, the operator are acknowledged on
+                  the website.
+                </li>
+                <li>
+                  Unauthorized use of information provided by us shall give rise
+                  to a claim for damages and/or be a criminal offense.
+                </li>
+                <li>
+                  From time to time our website may also include links to other
+                  websites. These links are provided for your convenience to
+                  provide further information.
+                </li>
+                <li>
+                  You may not create a link to our website from another website
+                  or document without Mnar Clothing's prior written consent.
+                </li>
+                <li>
+                  Any dispute arising out of use of our website and/or purchase
+                  with us and/or any engagement with us is subject to the laws
+                  of India .
+                </li>
+                <li>
+                  We, shall be under no liability whatsoever in respect of any
+                  loss or damage arising directly or indirectly out of the
+                  decline of authorization for any Transaction, on Account of
+                  the Cardholder having exceeded the preset limit mutually
+                  agreed by us with our acquiring bank from time to time
+                </li>
+              </ul>
+            </div>
 
-
-              </li>
-              <li>Unauthorized use of information provided by us shall give rise to a claim for damages and/or be a criminal offense.</li>
-              <li>From time to time our website may also include links to other websites. These links are provided for your convenience to provide further information.</li>
-              <li>You may not create a link to our website from another website or document without Mnar Clothing's prior written consent.</li>
-              <li>Any dispute arising out of use of our website and/or purchase with us and/or any engagement with us is subject to the laws of India .</li>
-              <li>We, shall be under no liability whatsoever in respect of any loss or damage arising directly or indirectly out of the decline of authorization for any Transaction, on Account of the Cardholder having exceeded the preset limit mutually agreed by us with our acquiring bank from time to time</li>
-            </ul>
+            <div>
+              <h2 className="text-2xl font-semibold text-gray-900">
+                2. Products and Pricing
+              </h2>
+              <ul className="list-disc list-inside text-lg mt-2">
+                <li>
+                  All product descriptions, pricing, and availability are
+                  subject to change without notice.
+                </li>
+                <li>
+                  We strive to provide accurate product information, but we do
+                  not warrant that descriptions or prices are free of errors.
+                </li>
+                <li>
+                  In the case of incorrect pricing, we reserve the right to
+                  cancel any affected orders.
+                </li>
+              </ul>
+            </div>
           </div>
-
-          <div>
-            <h2 className="text-2xl font-semibold text-gray-900">
-              2. Products and Pricing
-            </h2>
-            <ul className="list-disc list-inside text-lg mt-2">
-              <li>
-                All product descriptions, pricing, and availability are subject
-                to change without notice.
-              </li>
-              <li>
-                We strive to provide accurate product information, but we do not
-                warrant that descriptions or prices are free of errors.
-              </li>
-              <li>
-                In the case of incorrect pricing, we reserve the right to cancel
-                any affected orders.
-              </li>
-            </ul>
-          </div>
-
         </div>
-       </div>
-     
-       <div>
-       <h1 className="text-4xl font-bold text-center mb-6 mt-6 text-gray-900">
-       Cancellation & Refund Policy
-        </h1>
-        <p className="mb-4 text-2xl text-gray-600">Last updated on Apr 1st 2025</p>
-        <p className="mb-4 text-lg">
-        Mnar Clothing believes in helping its customers as far as possible, and has therefore a liberal cancellation policy. Under this policy:
-        </p>
 
-        <div className="space-y-6">
-          <div>
-            <h2 className="text-2xl font-semibold text-gray-900">
-                
-            </h2>
-            <ul className="list-disc list-inside text-lg mt-2">
-              <li>
-              Cancellations will be considered only if the request is made within 2-3 days of placing the order. However, the cancellation request may not be entertained if the orders have been communicated to the vendors/merchants and they have initiated the process of shipping them.
+        <div>
+          <h1 className="text-4xl font-bold text-center mb-6 mt-6 text-gray-900">
+            Cancellation & Refund Policy
+          </h1>
+          <p className="mb-4 text-2xl text-gray-600">
+            Last updated on Apr 1st 2025
+          </p>
+          <p className="mb-4 text-lg">
+            Mnar Clothing believes in helping its customers as far as possible,
+            and has therefore a liberal cancellation policy. Under this policy:
+          </p>
 
+          <div className="space-y-6">
+            <div>
+              <h2 className="text-2xl font-semibold text-gray-900"></h2>
+              <ul className="list-disc list-inside text-lg mt-2">
+                <li>
+                  Cancellations will be considered only if the request is made
+                  within 2-3 days of placing the order. However, the
+                  cancellation request may not be entertained if the orders have
+                  been communicated to the vendors/merchants and they have
+                  initiated the process of shipping them.
+                </li>
+                <li>
+                  Mnar Clothing does not accept cancellation requests for
+                  perishable items like flowers, eatables etc. However,
+                  refund/replacement can be made if the customer establishes
+                  that the quality of product delivered is not good.
+                </li>
+                <li>
+                  In case of receipt of damaged or defective items please report
+                  the same to our Customer Service team. The request will,
+                  however, be entertained once the merchant has checked and
+                  determined the same at his own end. This should be reported
+                  within 2-3 days of receipt of the products.
+                </li>
+                <li>
+                  In case you feel that the product received is not as shown on
+                  the site or as per your expectations, you must bring it to the
+                  notice of our customer service within 2-3 days of receiving
+                  the product. The Customer Service Team after looking into your
+                  complaint will take an appropriate decision.
+                </li>
+                <li>
+                  In case of complaints regarding products that come with a
+                  warranty from manufacturers, please refer the issue to them.
+                </li>
+                <li>
+                  In case of any Refunds approved by the Mnar Clothing, it’ll
+                  take 3-4 days for the refund to be processed to the end
+                  customer.
+                </li>
+              </ul>
+            </div>
 
-              </li>
-              <li>Mnar Clothing does not accept cancellation requests for perishable items like flowers, eatables etc. However, refund/replacement can be made if the customer establishes that the quality of product delivered is not good.</li>
-              <li>
-              In case of receipt of damaged or defective items please report the same to our Customer Service team. The request will, however, be entertained once the merchant has checked and determined the same at his own end. This should be reported within 2-3 days of receipt of the products.
-              </li>
-              <li>
-              In case you feel that the product received is not as shown on the site or as per your expectations, you must bring it to the notice of our customer service within 2-3 days of receiving the product. The Customer Service Team after looking into your complaint will take an appropriate decision.
-              </li>
-              <li>
-              In case of complaints regarding products that come with a warranty from manufacturers, please refer the issue to them.
-
-
-
-
-              </li>
-              <li>In case of any Refunds approved by the Mnar Clothing, it’ll take 3-4 days for the refund to be processed to the end customer.</li>
-        
-            </ul>
+            <div>
+              <h2 className="text-4xl font-bold text-center mb-6 mt-6 text-gray-900">
+                Shipping & Delivery Policy
+              </h2>
+              <p className="mb-4 text-2xl text-gray-600">
+                Last updated on Apr 1st 2025
+              </p>
+              <ul className="list-disc list-inside text-lg mt-2">
+                <li>
+                  For International buyers, orders are shipped and delivered
+                  through registered international courier companies and/or
+                  International speed post only. For domestic buyers, orders are
+                  shipped through registered domestic courier companies and /or
+                  speed post only. Orders are shipped within 0-7 days or as per
+                  the delivery date agreed at the time of order confirmation and
+                  delivering of the shipment subject to Courier Company / post
+                  office norms. Mnar Clothing is not liable for any delay in
+                  delivery by the courier company / postal authorities and only
+                  guarantees to hand over the consignment to the courier company
+                  or postal authorities within 0-7 days rom the date of the
+                  order and payment or as per the delivery date agreed at the
+                  time of order confirmation. Delivery of all orders will be to
+                  the address provided by the buyer. Delivery of our services
+                  will be confirmed on your mail ID as specified during
+                  registration. For any issues in utilizing our services you may
+                  contact our helpdesk on 8821851060 or 0junedmansuri@gmail.com
+                </li>
+              </ul>
+            </div>
           </div>
-
-          <div>
-            <h2 className="text-4xl font-bold text-center mb-6 mt-6 text-gray-900">
-            Shipping & Delivery Policy
-            </h2>
-            <p className="mb-4 text-2xl text-gray-600">Last updated on Apr 1st 2025</p>
-            <ul className="list-disc list-inside text-lg mt-2">
-              <li>
-              For International buyers, orders are shipped and delivered through registered international courier companies and/or International speed post only. For domestic buyers, orders are shipped through registered domestic courier companies and /or speed post only. Orders are shipped within 0-7 days or as per the delivery date agreed at the time of order confirmation and delivering of the shipment subject to Courier Company / post office norms. Mnar Clothing is not liable for any delay in delivery by the courier company / postal authorities and only guarantees to hand over the consignment to the courier company or postal authorities within 0-7 days rom the date of the order and payment or as per the delivery date agreed at the time of order confirmation. Delivery of all orders will be to the address provided by the buyer. Delivery of our services will be confirmed on your mail ID as specified during registration. For any issues in utilizing our services you may contact our helpdesk on 8821851060 or 0junedmansuri@gmail.com
-              </li>
-         
-            </ul>
-          </div>
-
         </div>
-       </div>
 
         <h1 className="text-4xl font-bold text-center mt-10 mb-6 text-gray-900">
           Contact Us
@@ -128,15 +206,14 @@ const Terms = () => {
         </p>
         <ul className="list-disc list-inside text-lg mt-2">
           <li>
-            <span className="font-semibold">Email:</span>{" "}
-            mnarclothing@gmail.com
+            <span className="font-semibold">Email:</span> mnarclothing@gmail.com
           </li>
           <li>
             <span className="font-semibold">Phone:</span> +91 7804023523
           </li>
           <li>
-            <span className="font-semibold">Address:</span> kityani, Mandsaur,
-            Madhya Pradesh, India
+            <span className="font-semibold">Address:</span> 458001 , Building No - 47, Ward No . 11 , Chakrawati Colony, Near
+            Pamnani Hospital , Mandsaur , Madhya Pradesh
           </li>
         </ul>
       </div>
